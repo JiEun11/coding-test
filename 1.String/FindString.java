4e2709fec4678e206a7cbe4e77bb20bac8361a85
Bella <shdudtnr3939@naver.com>
2022-09-20 23:13:28 +0900

    section #1-1 with 2 ways

---
+++ 1.String/FindString.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class FindString {
+  public int solution(String str, char t) {
+    int answer = 0;
+    str = str.toUpperCase();
+    t = Character.toUpperCase(t);
+
+    // for (int i = 0; i < str.length(); i++) {
+    // if (str.charAt(i) == t)
+    // answer++;
+    // }
+    for (char x : str.toCharArray()) {
+      if (x == t)
+        answer++;
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    FindString fs = new FindString();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    char c = kb.next().charAt(0);
+    System.out.print(fs.solution(str, c));
+  }
+}
