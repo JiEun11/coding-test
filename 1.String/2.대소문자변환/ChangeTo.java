111253c1995eca8543542dd9be5ddd6ee3fa4cc8
Bella <shdudtnr3939@naver.com>
2022-09-27 21:51:16 +0900

    2.대소문자변환 풀이2

--- 1.String/2.대소문자변환/ChangeTo.java
+++ 1.String/2.대소문자변환/ChangeTo.java
@@ -4,11 +4,18 @@ public class ChangeTo {
 
   public String solution(String str) {
     String answer = "";
+    // A : 65 - 90
+    // a : 97 - 122
     for (char x : str.toCharArray()) {
-      if (Character.isLowerCase(x)) {
-        answer += Character.toUpperCase(x);
+      // if (Character.isLowerCase(x)) {
+      // answer += Character.toUpperCase(x);
+      // } else {
+      // answer += Character.toLowerCase(x);
+      // }
+      if (x >= 65 && x <= 90) {
+        answer += (x - 32); // 소문자 -> 대문자
       } else {
-        answer += Character.toLowerCase(x);
+        answer += (x + 32); // 대문자 -> 소문자
       }
     }
     return answer;
