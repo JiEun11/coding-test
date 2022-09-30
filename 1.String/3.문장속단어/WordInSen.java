27bf8e8fb739d03be2bd3e15c5a4348d376cb866
Bella <shdudtnr3939@naver.com>
2022-09-30 21:54:06 +0900

    3.문장속단어 indexOf 사용

--- 1.String/3.문장속단어/WordInSen.java
+++ 1.String/3.문장속단어/WordInSen.java
@@ -3,15 +3,23 @@ import java.util.*;
 public class WordInSen {
   public String solution(String str) {
     String answer = "";
-    int m = Integer.MIN_VALUE; // 가장 작은 값으로 초기화
-    String[] s = str.split(" ");
-    for (String x : s) {
-      int len = x.length();
+    int m = Integer.MIN_VALUE, pos; // 가장 작은 값으로 초기화
+    while ((pos = str.indexOf(' ')) != -1) { // 띄어쓰기가 처음으로 발견되는 2리턴
+      String tmp = str.substring(0, pos); // 0-4까지 잘라냄
+      int len = tmp.length();
       if (len > m) {
-        m = len; // 최댓값 구하는 알고리즘
-        answer = x;
+        m = len;
+        answer = tmp;
       }
     }
+    // String[] s = str.split(" ");
+    // for (String x : s) {
+    // int len = x.length();
+    // if (len > m) {
+    // m = len; // 최댓값 구하는 알고리즘
+    // answer = x;
+    // }
+    // }
 
     return answer;
   }
