b044873009cc0f82b2abb9f48640cafefa864f4e
Bella <shdudtnr3939@naver.com>
2022-10-04 23:33:40 +0900

    4.단어뒤집기 #1

---
+++ 1.String/4.단어뒤집기/ReverseWord.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ReverseWord {
+  public ArrayList<String> solution(int n, String[] str) {
+    ArrayList<String> answer = new ArrayList<>();
+    for (String x : str) {
+      String tmp = new StringBuilder(x).reverse().toString();
+      answer.add(tmp);
+    }
+    return answer;
+
+  }
+
+  public static void main(String[] args) {
+    ReverseWord T = new ReverseWord();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    String[] str = new String[n];
+    for (int i = 0; i < n; i++) {
+      str[i] = kb.next();
+    }
+    for (String x : T.solution(n, str)) {
+      System.out.println(x);
+    }
+  }
+
+}
