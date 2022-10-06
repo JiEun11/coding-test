19fbbafd0896abda5494483041210d34834b08a4
Bella <shdudtnr3939@naver.com>
2022-10-06 21:05:41 +0900

    6.중복문자제거 indexOf

---
+++ 1.String/6.중복문자제거/RemoveDupl.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RemoveDupl {
+
+  public String solution(String str) {
+    String answer = "";
+    for (int i = 0; i < str.length(); i++) {
+      // System.out.println(str.charAt(i) + " " + i + " " +
+      // str.indexOf(str.charAt(i)));
+      if (str.indexOf(str.charAt(i)) == i) {
+        answer += str.charAt(i);
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    RemoveDupl T = new RemoveDupl();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    System.out.print(T.solution(str));
+  }
+}
