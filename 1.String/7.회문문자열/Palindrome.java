0b32ab57ee4e09e293a9480e79b71b2639146485
Bella <shdudtnr3939@naver.com>
2022-10-10 22:40:46 +0900

    7.회문문자열 StringBuilder 사용 비교

--- 1.String/7.회문문자열/Palindrome.java
+++ 1.String/7.회문문자열/Palindrome.java
@@ -3,14 +3,19 @@ import java.util.*;
 public class Palindrome {
 
   public String solution(String str) {
-    String answer = "YES";
-    str = str.toUpperCase();
-    int len = str.length();
-    for (int i = 0; i < len / 2; i++) {
-      if (str.charAt(i) != str.charAt(len - i - 1))
-        return "NO";
-    }
+    String answer = "NO";
+    String tmp = new StringBuilder(str).reverse().toString();
+    if (str.equalsIgnoreCase(tmp))
+      answer = "YES";
+    // String answer = "YES";
+    // str = str.toUpperCase();
+    // int len = str.length();
+    // for (int i = 0; i < len / 2; i++) {
+    // if (str.charAt(i) != str.charAt(len - i - 1))
+    // return "NO";
+    // }
     return answer;
+
   }
 
   public static void main(String[] args) {
