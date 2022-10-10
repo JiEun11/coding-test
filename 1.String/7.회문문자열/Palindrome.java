72182c1b5e1dce8a184139f22dd12dad319b4fb9
Bella <shdudtnr3939@naver.com>
2022-10-10 21:59:59 +0900

    7.회문문자열 charAt() 비교

---
+++ 1.String/7.회문문자열/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Palindrome {
+
+  public String solution(String str) {
+    String answer = "YES";
+    str = str.toUpperCase();
+    int len = str.length();
+    for (int i = 0; i < len / 2; i++) {
+      if (str.charAt(i) != str.charAt(len - i - 1))
+        return "NO";
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Palindrome T = new Palindrome();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    System.out.println(T.solution(str));
+  }
+}
