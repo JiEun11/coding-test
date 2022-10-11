bac6f4eea994be438d19e2eda77b3666de727dee
Bella <shdudtnr3939@naver.com>
2022-10-11 21:12:14 +0900

    9.숫자만 추출 : Character.isDigit 사용

--- 1.String/9.숫자만추출/ExtractNum.java
+++ 1.String/9.숫자만추출/ExtractNum.java
@@ -3,14 +3,18 @@ import java.util.*;
 public class ExtractNum {
 
   public int solution(String s) {
-    int answer = 0;
+    // int answer = 0;
+    String answer = "";
     for (char x : s.toCharArray()) { // 문자열 배열로 만들기
-      if (x >= 48 && x <= 57) { // 이 사이에 있다는 건 문자라는 의미
-        answer = answer * 10 + (x - 48);
+      // if (x >= 48 && x <= 57) { // 이 사이에 있다는 건 문자라는 의미
+      // answer = answer * 10 + (x - 48);
+      // }
+      if (Character.isDigit(x)) {
+        answer += x;
       }
     }
 
-    return answer;
+    return Integer.parseInt(answer); // parseInt하면 0208 -> 208이 된다.
   }
 
   public static void main(String[] args) {
