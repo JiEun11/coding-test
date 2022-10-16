865bff0d9670ccf071d5823fb3bafedbb89b1410
Bella <shdudtnr3939@naver.com>
2022-10-16 11:49:51 +0900

    add comments

--- 2.Array/1.큰수출력하기/PrintMaxNum.java
+++ 2.Array/1.큰수출력하기/PrintMaxNum.java
@@ -4,7 +4,7 @@ class PrintMaxNum {
 
   public ArrayList<Integer> solution(int n, int[] arr) {
     ArrayList<Integer> answer = new ArrayList<>();
-    answer.add(arr[0]);
+    answer.add(arr[0]); // 첫 번째 수는 무조건 출력
     for (int i = 1; i < n; i++) {
       if (arr[i] > arr[i - 1]) {
         answer.add(arr[i]);
