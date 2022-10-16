2f9a0024d26528a57a22df963cb7fd15dc7fea6e
Bella <shdudtnr3939@naver.com>
2022-10-16 12:16:52 +0900

    2.보이는 학생

---
+++ 2.Array/2.보이는학생/SeeStudents.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SeeStudents {
+
+  public int solution(int n, int[] arr) {
+    int answer = 1;
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+      if (arr[i] > max) {
+        answer++;
+        max = arr[i];
+      }
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    SeeStudents T = new SeeStudents();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = kb.nextInt();
+    }
+    System.out.print(T.solution(n, arr));
+
+    kb.close();
+  }
+}
