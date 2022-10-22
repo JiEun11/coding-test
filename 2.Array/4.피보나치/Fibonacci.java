ea09febe7a46dc4c44d86902ca850d68e6ca0c92
Bella <shdudtnr3939@naver.com>
2022-10-22 18:37:14 +0900

    [Add] Fibonacci by Manual coding

--- 2.Array/4.피보나치/Fibonacci.java
+++ 2.Array/4.피보나치/Fibonacci.java
@@ -3,10 +3,18 @@ import java.util.*;
 public class Fibonacci {
   public int[] solution(int n) {
     int[] answer = new int[n];
-    answer[0] = 1;
-    answer[1] = 1;
-    for (int i = 2; i < n; i++) {
-      answer[i] = answer[i - 2] + answer[i - 1];
+//    answer[0] = 1;
+//    answer[1] = 1;
+//    for (int i = 2; i < n; i++) {
+//      answer[i] = answer[i - 2] + answer[i - 1];
+//    }
+    int a = 1, b = 1, c;
+    System.out.print(a + " " + b + " ");
+    for(int i = 2; i < n; i++){
+      c = a + b;
+      System.out.print(c + " ");
+      a = b;
+      b = c;
     }
     return answer;
   }
@@ -15,8 +23,9 @@ public class Fibonacci {
     Fibonacci T = new Fibonacci();
     Scanner kb = new Scanner(System.in);
     int n = kb.nextInt();
-    for (int x : T.solution(n))
-      System.out.print(x + " ");
+    T.solution(n);
+//    for (int x : T.solution(n))
+//      System.out.print(x + " ");
     kb.close();
   }
 }
