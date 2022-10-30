96a607fa140652ea3adeb0d54e08c61b2e071238
Bella <shdudtnr3939@naver.com>
2022-10-30 19:31:29 +0900

    [Add] 최대 매출 with Sliding window

---
+++ 3.TwoPointers/3.최대매출/MaxSales.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MaxSales {
+
+    public int solution(int n , int k, int[] arr){
+        int answer, sum = 0 ;
+
+        for(int i = 0; i<k; i++){
+            sum+= arr[i];
+        }
+        answer = sum;
+        for(int i=k; i<n; i++){
+            sum+= (arr[i]-arr[i-k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        MaxSales T = new MaxSales();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i =0 ; i<n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, k, arr));
+
+        kb.close();
+    }
+}
