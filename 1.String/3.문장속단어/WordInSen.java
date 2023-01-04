84e3c2e2f23c7d48aab83c676b411192be7bf5c8
Bella <shdudtnr3939@naver.com>
2023-01-04 22:36:52 +0900

    [완료] 문장 속 단어 정답 #2 - indexOf substring 사용

--- 1.String/3.문장속단어/WordInSen.java
+++ 1.String/3.문장속단어/WordInSen.java
@@ -4,15 +4,28 @@ public class WordInSen {
   public String solution(String str) {
     String answer = "";
     int lengthCount = Integer.MIN_VALUE;
-    String[] wordList = str.split(" ");
+    int pos = 0;
+    /*String[] wordList = str.split(" ");
     for(String word : wordList){
       int len = word.length();
       if(len > lengthCount){
         lengthCount = len;
         answer = word;
       }
-    }
+    }*/
 
+    while((pos=str.indexOf(' '))!= -1){ // 띄어쓰기 발견 안 되면 -1 return
+      String tmp = str.substring(0, pos);
+      int len = tmp.length();
+      if(len > lengthCount) {
+        lengthCount = len;
+        answer = tmp;
+      }
+      str = str.substring(pos+1);   // str 갱신
+    }
+    if(str.length() > lengthCount){
+      answer = str;
+    }
     return answer;
   }
 
