c204af017afcd1a0992a5adc92113083135d943f
Bella <shdudtnr3939@naver.com>
2023-01-09 23:00:05 +0900

    [add] 중복문자제거 - 풀기

--- 1.String/6.중복문자제거/RemoveDupl.java
+++ 1.String/6.중복문자제거/RemoveDupl.java
@@ -4,13 +4,13 @@ public class RemoveDupl {
 
   public String solution(String str) {
     String answer = "";
-    for (int i = 0; i < str.length(); i++) {
-      // System.out.println(str.charAt(i) + " " + i + " " +
-      // str.indexOf(str.charAt(i)));
-      if (str.indexOf(str.charAt(i)) == i) {
-        answer += str.charAt(i);
+    for(char word : str.toCharArray()){
+      String letter = Character.toString(word);
+      if(!answer.contains(letter)){
+        answer += letter;
       }
     }
+
     return answer;
   }
 
