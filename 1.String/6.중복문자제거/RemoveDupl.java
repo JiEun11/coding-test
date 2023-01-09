9b1b20c8b07febea697e2c60cc627503826eb5d5
Bella <shdudtnr3939@naver.com>
2023-01-09 23:04:43 +0900

    [MODIFIED] 중복문자제거 - 쌤 답

--- 1.String/6.중복문자제거/RemoveDupl.java
+++ 1.String/6.중복문자제거/RemoveDupl.java
@@ -4,10 +4,17 @@ public class RemoveDupl {
 
   public String solution(String str) {
     String answer = "";
-    for(char word : str.toCharArray()){
-      String letter = Character.toString(word);
-      if(!answer.contains(letter)){
-        answer += letter;
+//    for(char word : str.toCharArray()){
+//      String letter = Character.toString(word);
+//      if(!answer.contains(letter)){
+//        answer += letter;
+//      }
+//    }
+
+    for(int i = 0; i<str.length(); i++){
+//      System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+      if(str.indexOf(str.charAt(i))==i){
+        answer += str.charAt(i);
       }
     }
 
