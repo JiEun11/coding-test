0946ee06d26e619660142c6ce0d83f31150f9ed0
Bella <shdudtnr3939@naver.com>
2023-01-11 00:08:23 +0900

    [ADD] 회문문자열 - 내 답

--- 1.String/7.회문문자열/PalindromeString.java
+++ 1.String/7.회문문자열/PalindromeString.java
@@ -4,16 +4,21 @@ public class PalindromeString {
 
   public String solution(String str) {
     String answer = "NO";
-    String tmp = new StringBuilder(str).reverse().toString();
-    if (str.equalsIgnoreCase(tmp))
-      answer = "YES";
-    // String answer = "YES";
-    // str = str.toUpperCase();
-    // int len = str.length();
-    // for (int i = 0; i < len / 2; i++) {
-    // if (str.charAt(i) != str.charAt(len - i - 1))
-    // return "NO";
-    // }
+    int lt = 0;
+    int rt = str.length()-1;
+    String[] strArr = str.split("");
+    while(lt < rt){
+      String left = strArr[lt].toLowerCase();
+      String right = strArr[rt].toLowerCase();
+
+      if(left.equals(right)){
+        lt++;
+        rt--;
+        answer = "YES";
+      }else{
+        break;
+      }
+    }
     return answer;
 
   }
