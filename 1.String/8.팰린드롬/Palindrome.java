b6a0ac17b18bff600c7b3cd1001402eda37430df
Bella <shdudtnr3939@naver.com>
2023-01-12 23:52:54 +0900

    [MODIFIED] 펠린드롬 - 쎔 답 (replaceAll 정규식 및 StringBuilder 이용)

--- 1.String/8.팰린드롬/Palindrome.java
+++ 1.String/8.팰린드롬/Palindrome.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 public class Palindrome {
   public String Solution(String s) {
-    String answer = "YES";
-    String refinedStr = s.replaceAll("[^a-zA-Z]","");
-    System.out.println(refinedStr);
-    refinedStr = refinedStr.toLowerCase();
-    int len = refinedStr.length();
-    for(int i = 0; i < len/2; i++){
-      if(refinedStr.charAt(i) != refinedStr.charAt(len-i-1)) return "NO";
-    }
+//    String answer = "YES";
+//    String refinedStr = s.replaceAll("[^a-zA-Z]","");
+//    refinedStr = refinedStr.toLowerCase();
+//    int len = refinedStr.length();
+//    for(int i = 0; i < len/2; i++){
+//      if(refinedStr.charAt(i) != refinedStr.charAt(len-i-1)) return "NO";
+//    }
+    String answer = "NO";
+    s = s.toUpperCase().replaceAll("[^A-Z]", "");
+    String tmp = new StringBuilder(s).reverse().toString();
+    if(s.equals(tmp)) answer="YES";
     return answer;
   }
 
