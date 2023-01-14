b4a6c27da704c546dc8c5614a5af8ade7e837f34
Bella <shdudtnr3939@naver.com>
2023-01-14 15:36:46 +0900

    [modified] 숫자만 추출- 쌤 답 2가지

--- 1.String/9.숫자만추출/ExtractNum.java
+++ 1.String/9.숫자만추출/ExtractNum.java
@@ -3,14 +3,29 @@ import java.util.*;
 public class ExtractNum {
 
   public int solution(String s) {
-    String answer = "";
+    /*String answer = "";
     for(char c : s.toCharArray()){
       if(47 <= c && c <= 57){
         answer += c;
       }
     }
+    return I*/nteger.parseInt(answer); // parseInt하면 0208 -> 208이 된다.
 
-    return Integer.parseInt(answer); // parseInt하면 0208 -> 208이 된다.
+    /*int answer = 0;
+    for(char x : s.toCharArray()){
+      if(x>=48 && x<=57){
+        answer = answer * 10 + (x-48);
+      }
+    }
+    return answer;*/
+
+    String answer = "";
+    for(char x : s.toCharArray()){
+      if(Character.isDigit(x)){
+        answer += x;
+      }
+    }
+    return Integer.parseInt(answer);
   }
 
   public static void main(String[] args) {
