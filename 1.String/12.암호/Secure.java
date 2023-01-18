69f8acf3bda7925318fe7f558f9019123b91c3b9
Bella <shdudtnr3939@naver.com>
2023-01-18 23:33:05 +0900

    [ADD]암호 내 답

--- 1.String/12.암호/Secure.java
+++ 1.String/12.암호/Secure.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Secure {
   public String solution(int n, String s) {
     String answer = "";
-    for (int i = 0; i < n; i++) {
-      String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
-      int num = Integer.parseInt(tmp, 2);
-      answer += (char) num;
-      System.out.println(tmp + " " + num);
-      s = s.substring(7);
+    for(int i = 0; i < n*7; i+=7){
+      String splitStr = s.substring(i,i+7);
+      String BinaryStr = splitStr.replace("#","1").replace("*","0");
+      int asciiNum = Integer.valueOf(BinaryStr, 2);
+      char ch = (char)asciiNum;
+      answer += String.valueOf(ch);
     }
     return answer;
   }
