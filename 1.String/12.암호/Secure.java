2259df55d7b9e057dc5ac2c4dbbc05143a3303c0
Bella <shdudtnr3939@naver.com>
2023-01-19 23:28:35 +0900

    [ADD]큰수출력하기 - 내 답

--- 1.String/12.암호/Secure.java
+++ 1.String/12.암호/Secure.java
@@ -3,12 +3,18 @@ import java.util.*;
 public class Secure {
   public String solution(int n, String s) {
     String answer = "";
-    for(int i = 0; i < n*7; i+=7){
+    /*for(int i = 0; i < n*7; i+=7){
       String splitStr = s.substring(i,i+7);
       String BinaryStr = splitStr.replace("#","1").replace("*","0");
       int asciiNum = Integer.valueOf(BinaryStr, 2);
       char ch = (char)asciiNum;
       answer += String.valueOf(ch);
+    }*/
+    for(int i=0; i<n; i++){
+      String tmp = s.substring(0,7).replace('#','1').replace('*','0');
+      int num = Integer.parseInt(tmp, 2);
+      answer += (char)num;
+      s = s.substring(7);
     }
     return answer;
   }
--- 2.Array/1.큰수출력하기/PrintMaxNum.java
+++ 2.Array/1.큰수출력하기/PrintMaxNum.java
@@ -4,10 +4,10 @@ class PrintMaxNum {
 
   public ArrayList<Integer> solution(int n, int[] arr) {
     ArrayList<Integer> answer = new ArrayList<>();
-    answer.add(arr[0]); // 첫 번째 수는 무조건 출력
-    for (int i = 1; i < n; i++) {
-      if (arr[i] > arr[i - 1]) {
-        answer.add(arr[i]);
+    answer.add(arr[0]); // 첫 번째 수는 무조건 넣어주기
+    for(int i = 0; i < n ; i++){
+      if(arr[i]<arr[i+1]){
+        answer.add(arr[i+1]);
       }
     }
 
