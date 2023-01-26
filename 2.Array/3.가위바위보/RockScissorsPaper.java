9fefae85ad02c3ead079a950b187d5584c477e33
Bella <shdudtnr3939@naver.com>
2023-01-26 22:45:16 +0900

    [ADD] 가위바위보 - 내 답

--- 2.Array/3.가위바위보/RockScissorsPaper.java
+++ 2.Array/3.가위바위보/RockScissorsPaper.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class RockScissorsPaper {
   public String solution(int n, int[] a, int[] b) {
     String answer = "";
-    for (int i = 0; i < n; i++) {
-      if (a[i] == b[i])
-        answer += "D";
-      else if (a[i] == 1 && b[i] == 3)
-        answer += "A";
-      else if (a[i] == 2 && b[i] == 1)
-        answer += "A";
-      else if (a[i] == 3 && b[i] == 2)
-        answer += "A";
-      else
-        answer += "B";
+
+    for(int i = 0; i < n; i++){
+      if(a[i]==1 && b[i]==3 || a[i]==2 && b[i]==1 || a[i]==3 && b[i]==2){
+        answer+= "A";
+      }else if(a[i]==b[i]){ //비겼을 경우
+        answer+="D";
+      }else if(a[i]==1 && b[i]==2 || a[i]==2 && b[i]==3 || a[i]==3 && b[i]==1){
+        answer+= "B";
+      }
+
     }
+
     return answer;
   }
 
