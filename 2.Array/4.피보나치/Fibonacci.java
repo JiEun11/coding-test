a6fe699638f1de1164a813618cb838fc8fb11cda
Bella <shdudtnr3939@naver.com>
2023-01-27 23:28:32 +0900

    [ADD] 피보나치수열 - 내 답

--- 2.Array/4.피보나치/Fibonacci.java
+++ 2.Array/4.피보나치/Fibonacci.java
@@ -3,18 +3,11 @@ import java.util.*;
 public class Fibonacci {
   public int[] solution(int n) {
     int[] answer = new int[n];
-//    answer[0] = 1;
-//    answer[1] = 1;
-//    for (int i = 2; i < n; i++) {
-//      answer[i] = answer[i - 2] + answer[i - 1];
-//    }
-    int a = 1, b = 1, c;
-    System.out.print(a + " " + b + " ");
-    for(int i = 2; i < n; i++){
-      c = a + b;
-      System.out.print(c + " ");
-      a = b;
-      b = c;
+    answer[0] = 1;
+    answer[1] = 1;
+
+    for(int i=2; i<n; i++){
+      answer[i] = answer[i-2] + answer[i-1];
     }
     return answer;
   }
@@ -24,8 +17,8 @@ public class Fibonacci {
     Scanner kb = new Scanner(System.in);
     int n = kb.nextInt();
     T.solution(n);
-//    for (int x : T.solution(n))
-//      System.out.print(x + " ");
+    for (int x : T.solution(n))
+      System.out.print(x + " ");
     kb.close();
   }
 }
