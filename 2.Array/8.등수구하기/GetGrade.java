87e58c044e0a00e1c5da1e94e891fc586b52c1fc
Bella <shdudtnr3939@naver.com>
2023-02-14 00:09:24 +0900

    [ADD] 등수구하기 (쌤 답, 해결 못 함)

--- 2.Array/8.등수구하기/GetGrade.java
+++ 2.Array/8.등수구하기/GetGrade.java
@@ -4,12 +4,15 @@ public class GetGrade {
 
     public int[] solution(int n, int[] arr){
         int[] answer = new int[n];
-        for(int i=0; i<n; i++){
+        int tmp;
+        for(int i = 0; i < n; i++){
             int cnt = 1;
-            for(int j = 0; j<n; j++){
-                if(arr[j] > arr[i]) cnt++;
+            for(int j = 0 ; j < n ; j++){
+                if(arr[i] < arr[j]){
+                    cnt++;
+                }
+                answer[i] = cnt;
             }
-            answer[i] = cnt;
         }
         return answer;
     }
